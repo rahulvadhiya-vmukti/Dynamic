5b33c929d1bda33fc0dab12687b28f9ffb8d0c64
unknown <rahulvadhiya.vmukti@gmail.com>
2022-07-28 18:44:51 +0530

    more changes

--- routers/admin.js
+++ routers/admin.js
@@ -9,7 +9,7 @@ const { Product } = require("../models/product");
 
 const { User } = require("../models/user");
 const { Contact } = require("../models/contact");
-const { Order } = require("../models/order");
+
 const mongoose = require("mongoose");
 const multer = require("multer");
 const isAuth = require("../build/is-auth");
@@ -20,9 +20,9 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 
 cloudinary.config({ 
-	cloud_name: 'dfsixliv3', 
-	api_key: '823464447974964', 
-	api_secret: 'Loo3pVEXOqMMU7RnvJwRM5RjZug' 
+	cloud_name: 'alliance2701', 
+	api_key: '188895954541616', 
+	api_secret: 'Dic2Qj-TNUbkuWnhjAR6uBhU2-0' 
   });
 
 const fileTypeMap = {
@@ -61,7 +61,7 @@ const storage = new CloudinaryStorage({
 const upload = multer({ storage: storage });
 
 router.get(`/`, isAuth, async (req, res) => {
-	const order = await Order.countDocuments();
+	// const order = await Order.countDocuments();
 	const product = await Product.countDocuments();
 	const category = await Category.countDocuments();
 	const subcategory = await subCategory.countDocuments();
@@ -69,7 +69,7 @@ router.get(`/`, isAuth, async (req, res) => {
 	const contact = await Contact.countDocuments();
 
 	res.render("admin/index", {
-		orders: order,
+		// orders: order,
 		product: product,
 		category: category,
 		subcategory: subcategory,
@@ -90,14 +90,18 @@ router.get(`/category`, isAuth, async (req, res) => {
 	});
 });
 
+router.get(`/addcategory`, isAuth, async (req, res) => {
+
+	res.render("admin/addcategory", );
+});
 
 
-router.post(`/category`, isAuth,upload.single("image"), async (req, res) => {
+router.post(`/addcategory`, upload.single("image"),isAuth, async (req, res) => {
 	const fileName = req.file.filename;
-	const basePath = `https://res.cloudinary.com/dfsixliv3/image/upload/v1657608669/`;
-
+	const basePath = `https://res.cloudinary.com/alliance2701/image/upload/v1658992130/`;
+	
 	let category = new Category({
-		name: req.body.category,
+		name: req.body.name,
 		image: `${basePath}${fileName}`,
 	});
 
@@ -106,9 +110,37 @@ router.post(`/category`, isAuth,upload.single("image"), async (req, res) => {
 	if (!category)
 		res.status(500).send("This category was not sent to database...");
 
-	res.redirect("/admin/category");
+	res.redirect("/admin/addcategory");
 });
 
+
+router.get("/updatecategory", isAuth, async (req, res) => {
+	const category = await Category.findOne({ _id: req.query.id }).populate(
+		// "subcategory","product"
+	);
+	// const subcategory = await subCategory.find().select("name");
+	// const product = await Product.find().select("name");
+
+	res.render("admin/updatecategory", {
+		// subcategory: subcategory,
+		category: category,
+		// product: product,
+	});
+});
+
+
+
+
+
+
+
+router.get("/subcategory", isAuth, async (req, res) => {
+	const subcategory = await subCategory.find();
+
+	res.render("admin/subcategory", {
+		subcategory: subcategory,
+	});
+});
 
 router.get(`/addsubcategory`, isAuth, async (req, res) => {
 	const category = await Category.find().select("name");
@@ -136,16 +168,29 @@ router.post(`/addsubcategory`, isAuth,  async (req, res) => {
 });
 
 
+router.get("/updatesubcategory", isAuth, async (req, res) => {
+	const subcategory = await subCategory.findOne({ _id: req.query.id }).populate(
+		"category"
+	);
+	
+	const category = await Category.find().select("name");
 
-
-router.get("/subcategory", isAuth, async (req, res) => {
-	const subcategory = await subCategory.find();
-
-	res.render("admin/subcategory", {
+	res.render("admin/updatesubcategory", {
 		subcategory: subcategory,
+		category: category,
+		
 	});
 });
 
+
+
+router.get("/product", isAuth, async (req, res) => {
+	const product = await Product.find();
+
+	res.render("admin/product", {
+		product: product,
+	});
+});
 
 router.get(`/addproduct`, isAuth, async (req, res) => {
 	const category = await Category.find().select("name");
@@ -155,7 +200,7 @@ router.get(`/addproduct`, isAuth, async (req, res) => {
 
 router.post(`/addproduct`, isAuth, upload.single("image"), async (req, res) => {
 	const fileName = req.file.filename;
-	const basePath = `https://res.cloudinary.com/dfsixliv3/image/upload/v1657608669/`;
+	const basePath = `https://res.cloudinary.com/alliance2701/image/upload/v1658992130/`;
 
 	let product = new Product({
 		name: req.body.name,
@@ -174,10 +219,17 @@ router.post(`/addproduct`, isAuth, upload.single("image"), async (req, res) => {
 	res.redirect("/admin/addproduct");
 });
 
-router.get("/product", isAuth, async (req, res) => {
-	const product = await Product.find();
 
-	res.render("admin/product", {
+router.get("/updateproduct", isAuth, async (req, res) => {
+	const product = await Product.findOne({ _id: req.query.id }).populate(
+		"subcategory","category"
+	);
+	const subcategory = await subCategory.find().select("name");
+	const category = await Category.find().select("name");
+
+	res.render("admin/updateproduct", {
+		subcategory: subcategory,
+		category: category,
 		product: product,
 	});
 });
@@ -194,47 +246,10 @@ router.get("/productdetails", isAuth, async (req, res) => {
 	});
 });
 
-router.get("/updateproduct", isAuth, async (req, res) => {
-	const product = await Product.findOne({ _id: req.query.id }).populate(
-		"subcategory","category"
-	);
-	const subcategory = await subCategory.find().select("name");
-	const category = await Category.find().select("name");
 
-	res.render("admin/updateproduct", {
-		subcategory: subcategory,
-		category: category,
-		product: product,
-	});
-});
 
-router.get("/updatesubcategory", isAuth, async (req, res) => {
-	const subcategory = await subCategory.findOne({ _id: req.query.id }).populate(
-		"category"
-	);
-	
-	const category = await Category.find().select("name");
 
-	res.render("admin/updatesubcategory", {
-		subcategory: subcategory,
-		category: category,
-		
-	});
-});
 
-router.get("/updatecategory", isAuth, async (req, res) => {
-	const category = await Category.findOne({ _id: req.query.id }).populate(
-		// "subcategory","product"
-	);
-	// const subcategory = await subCategory.find().select("name");
-	// const product = await Product.find().select("name");
-
-	res.render("admin/updatecategory", {
-		// subcategory: subcategory,
-		category: category,
-		// product: product,
-	});
-});
 
 
 
@@ -253,7 +268,7 @@ router.post(
 
 		if (file) {
 			const fileName = req.file.filename;
-			const basePath = `https://res.cloudinary.com/dfsixliv3/image/upload/v1657608669/`;
+			const basePath = `https://res.cloudinary.com/alliance2701/image/upload/v1658992130/`;
 			imagePath = `${basePath}${fileName}`;
 		} else {
 			const product = await Product.findById(req.params.id);
@@ -286,7 +301,7 @@ router.post(
 	upload.single("image"),
 	async (req, res) => {
 		if (!mongoose.isValidObjectId(req.params.id)) {
-			return res.status(400).send("Invalid product id");
+			return res.status(400).send("Invalid Category id");
 		}
 
 		const file = req.file;
@@ -294,7 +309,7 @@ router.post(
 
 		if (file) {
 			const fileName = req.file.filename;
-			const basePath = `https://res.cloudinary.com/dfsixliv3/image/upload/v1657608669/`;
+			const basePath = `https://res.cloudinary.com/alliance2701/image/upload/v1658992130/`;
 			imagePath = `${basePath}${fileName}`;
 		} else {
 			const category = await Category.findById(req.params.id);
@@ -324,22 +339,22 @@ router.post(
 
 
 
-router.get("deleteproduct/:id", isAuth,async (req, res) => {
-	try {
-		const product = await Product.findByIdAndRemove(req.params.id);
-			
-		if (product) {
-			cloudinary.v2.uploader.destroy(`${product.image}`);
+router.get("/deletecategory/:id", isAuth,async (req, res) => {
+	
+		const category = await Category.findByIdAndRemove(req.params.id);
+			console.log("Removed")
+		if (category) {
+			await cloudinary.v2.uploader.destroy(`${category.image}`);
+			await category.remove()
 			// fileHelper.deleteFile(`public/${product.image}`);
-			return res.status(200).redirect("/admin/product");
+			return res.status(200).redirect("/admin/category");
 		} else {
 			return res
 				.status(404)
-				.json({ success: false, message: "The logoproduct not found" });
+				.json({ success: false, message: "The Category not found" });
 		}
-	} catch (err) {
-		return res.status(400).json({ success: false, error: err });
-	}
+
+		
 });
 
 
@@ -351,23 +366,6 @@ router.get("deleteproduct/:id", isAuth,async (req, res) => {
 
 
 
-router.get(`/mailbox`, isAuth, async (req, res) => {
-	const contact = await Contact.find();
 
-	res.render("admin/mailbox", {
-		messages: contact,
-	});
-});
-
-router.get(`/readmail`, isAuth, async (req, res) => {
-	const message = await Contact.findOne({ _id: req.query.id });
-	const findPath = req.query.path;
-	const fileText = req.query.text;
-	// message.filePath
-
-	res.render("admin/readmail", {
-		message: message,
-	});
-});
 
 module.exports = router;
